c443f1fc8242e89b5434c0a2437b6b9d4c1c260a
Przybylski 중광 <r351574nc3@gmail.com>
2010-02-09 17:58:11 -0700

    Adding perl modules and scripts for orthogal

---
+++ perl/blast_calc1.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+
+use strict;
+use lib 'modules/IPIG/';
+
+BEGIN {
+    require "BlastParser.class";
+    import BlastParser;
+    require "BlastRecordHandler.class";
+    import BlastRecordHandler;
+}
+
+
+
+package Main;
+
+use strict;
+
+use warnings;
+
+use base 'Exporter';
+
+our @EXPORT = qw(main);
+
+sub new {
+    my $class = shift;
+    return bless {}, $class;
+}
+
+sub main {
+    my $parser = new BlastParser(new BlastRecordHandler());
+    $parser->parse(pop(@ARGV));
+}
+
+Main::main();
