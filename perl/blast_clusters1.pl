19a0b15da58043a43a4841d7a543987955c2b791
Przybylski 중광 <r351574nc3@gmail.com>
2010-02-15 16:25:57 -0700

    Updating documentation

--- perl/blast_clusters1.pl
+++ perl/blast_clusters1.pl
@@ -54,8 +54,17 @@ sub graph {
         x_label           => 'Cluster Groups',
         y_label           => 'Clusters/Group',
         title             => 'Gene Cluster Cardinality',
+        bar_spacing       => 18,
+        bar_shadow        => 9,
+        shadowclr         => 'dred'
         );
-    my $gd = $graph->plot($clusters->clusters());
+    $graph->set_title_font(['/usr/share/fonts/truetype/msttcorefonts/arial.ttf'], 44);
+    $graph->set_x_label_font('/usr/share/fonts/truetype/msttcorefonts/arial.ttf', 30);
+    $graph->set_y_label_font('/usr/share/fonts/truetype/msttcorefonts/arial.ttf', 30);
+    $graph->set_values_font('/usr/share/fonts/truetype/msttcorefonts/arial.ttf', 20);
+    $graph->set_x_axis_font('/usr/share/fonts/truetype/msttcorefonts/arial.ttf', 20);
+    $graph->set_y_axis_font('/usr/share/fonts/truetype/msttcorefonts/arial.ttf', 20);
+    my $gd = $graph->plot($clusters->graph());
     
     open(IMG, '>file.png') or die $!;
     binmode IMG;
@@ -63,10 +72,39 @@ sub graph {
 }
 
 sub testGraph {
-    return (
-        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        );
+    my $clusters = new ClusterMatrix();
+
+    my $record1  = new BlastRecord("AP206_contig00001_778-11", "AP206_contig00001_778-11");
+    my $record2  = new BlastRecord("AP206_contig00001_778-11", "AP206_contig00001_778-12");
+    my $record3  = new BlastRecord("AP206_contig00001_778-11", "AP206_contig00001_778-13");
+    my $record4  = new BlastRecord("AP206_contig00001_778-11", "sicca_contig00170_13535-14302");
+    my $record5  = new BlastRecord("AP206_contig00001_778-11", "sicca_contig00170_13535-14303");
+    my $record6  = new BlastRecord("AP206_contig00001_778-11", "sicca_contig00170_13535-14304");
+    my $record7  = new BlastRecord("AP206_contig00001_778-15", "AP206_contig00001_778-15");
+    my $record8  = new BlastRecord("AP206_contig00001_778-15", "sicca_contig00170_13535-14305");
+    my $record9  = new BlastRecord("AP206_contig00001_778-15", "AP206_contig00001_778-16");
+    my $record10 = new BlastRecord("AP206_contig00001_778-15", "sicca_contig00170_13535-14306");
+    my $record11 = new BlastRecord("AP206_contig00001_778-15", "AP206_contig00001_778-17");
+    my $record12 = new BlastRecord("AP206_contig00001_778-15", "sicca_contig00170_13535-14307");
+    my $record13 = new BlastRecord("AP206_contig00001_778-15", "AP206_contig00001_778-18");
+    my $record14 = new BlastRecord("AP206_contig00001_778-15", "sicca_contig00170_13535-143028");
+                                  
+    $clusters->addEdge(new Edge($record1));
+    $clusters->addEdge(new Edge($record2));
+    $clusters->addEdge(new Edge($record3));
+    $clusters->addEdge(new Edge($record4));
+    $clusters->addEdge(new Edge($record5));
+    $clusters->addEdge(new Edge($record6));
+    $clusters->addEdge(new Edge($record7));
+    $clusters->addEdge(new Edge($record8));
+    $clusters->addEdge(new Edge($record9));
+    $clusters->addEdge(new Edge($record10));
+    $clusters->addEdge(new Edge($record11));
+    $clusters->addEdge(new Edge($record12));
+    $clusters->addEdge(new Edge($record13));
+    $clusters->addEdge(new Edge($record14));
+
+    return $clusters
 }
 
 sub main {
